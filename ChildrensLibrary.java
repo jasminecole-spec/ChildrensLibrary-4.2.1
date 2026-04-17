b325b6a1347238cc899fef71d47fe97744cb4f24
Jasmine <jasmine.cole@crpusd.org>
2026-04-16 22:30:49 -0700

    pltw part completed!

--- ChildrensLibrary.java
+++ ChildrensLibrary.java
@@ -10,5 +10,44 @@ public class ChildrensLibrary
   {
     Library library = new Library();
     ArrayList<Book> myarray = library.getChildrensBooks();
+    for (Book list : myarray)
+    {
+      if (list.getAuthor().equals("L. Frank Baum"))
+      {
+        System.out.println(list.getTitle());
+      }
+    }
+
+    String bookToFind = "Sky Island";
+    double minimumRating = 0.0;
+    for (Book list : myarray)
+    {
+      if (list.getTitle().equals(bookToFind))
+      {
+        minimumRating = list.getRating();
+      }
+    }
+    System.out.println(minimumRating);
+
+    for (Book list : myarray)
+    {
+      if (list.getRating() >= minimumRating)
+      {
+        String book = "";
+        String author = "";
+        book = list.getTitle();
+        author = list.getAuthor();
+        System.out.println("AUTHOR: " + author + " \n " + " BOOK: " + book + "\n");
+      }
+    }
+
+    for (int i = 0; i < myarray.size(); i++)
+    {
+      if (myarray.get(i).getTitle().contains("The"))
+      {
+        System.out.println("TITLE: " + myarray.get(i).getTitle());
+        System.out.println("LOCATION IN LIBRARY: " + i + "\n");
+      }
+    }
   }
 }
